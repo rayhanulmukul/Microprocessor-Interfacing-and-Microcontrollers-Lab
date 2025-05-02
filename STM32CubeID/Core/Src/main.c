d244e6262b441093260a78b2f8c3eae746cdce70
Md Rayhanul Islam <73143090+rayhanulmukul@users.noreply.github.com>
2025-05-02 19:10:23 +0600

    keil debugging

---
+++ STM32CubeID/Core/Src/main.c
@@ -0,0 +1,74 @@
+#include "main.h"
+
+void systick_init();
+void init();
+void display(char *message);
+void LCD_EnablePulse(void);
+void delay_ms(uint32_t delay);
+
+// ASCII values of A to Z
+//uint32_t lsb[] = {0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xA, 0xB, 0xC, 0xD, 0xE, 0xF, 0x0, 0x1, 0x2, 0x3, 0x4, 0x5, 0x6, 0x7, 0x8, 0x9, 0xA};
+//uint32_t msb[] = {0x4, 0x4, 0x4, 0x4, 0x4, 0x4, 0x4, 0x4, 0x4, 0x4, 0x4, 0x4, 0x4, 0x4, 0x4, 0x5, 0x5, 0x5, 0x5, 0x5, 0x5, 0x5, 0x5, 0x5, 0x5, 0x5, 0x5};
+
+
+int main(void)
+{
+	// Timer init
+	//systick_init();
+
+	// Enable the Alternate Function for PINs and Enable GPIOA
+		RCC->APB2ENR |= 5;
+
+		GPIOA->CRL &= ~(0xFFFFFFFF);   // Clear bits for PA0 - PA7
+    GPIOA->CRL |=  (0x33330033);   // Set mode and confuguration for PA0 - PA7
+
+		// Enable UART2
+		RCC->APB1ENR |=0x20000;
+
+		// Enable the related PINs. Set GPIOA 2 pin as TX and GRIOA 3 as RX
+		GPIOA->CRL |= (0xB00);
+		GPIOA->CRL |= (0x8000);
+
+
+		// Setup the baude rate for 9600 bps
+    uint32_t system_clock = 36000000; // 16 MHz (HSI)
+    uint32_t baud_rate = 9600;
+    USART2->BRR = (system_clock + (baud_rate / 2)) / baud_rate; // Correct BRR calculation
+
+		// Enable Uart Transmit
+		USART2->CR1 |= 8;
+
+		// Enable Uart Recive
+		USART2->CR1 |= 4;
+
+		// Enable Uart
+		USART2->CR1 |= 0x2000;
+
+		//set word length
+		USART2->CR1 |= 0x1000;
+
+
+while(1)
+	{
+		char chat;
+
+		// wait until data transmission is complete
+		while(!(USART2->SR & (1<<6)))
+		{};
+
+		//Receive data
+		chat = USART2->DR;
+
+		char message[2];
+
+		message[0]=chat;
+		message[1]='\0';
+
+		//Display data
+		//display(message);
+
+
+	}
+}
+// LCD display initialization function
+
---
+++ stm32cubeid_Sender/Core/Src/main.c
@@ -0,0 +1,49 @@
+#include "main.h"
+
+
+int main(void)
+{
+	// Enable the Alternate Function for PINs
+	RCC->APB2ENR |= 5;
+
+	// Enable UART2 clock
+	RCC->APB1ENR |=0x20000;
+
+		GPIOA->CRL &= ~(0xFFFFFFFF);   // Clear bits for PA0 - PA7
+    GPIOA->CRL |=  (0x33330033);   // Set mode and confuguration for PA0 - PA7
+
+	// Enable the related PINs. Set GPIOA 2 pin as TX and GRIOA 3 as RX
+		GPIOA->CRL |= (0xB00);
+		GPIOA->CRL |= (0x8000);
+
+	// Setup the baude rate for 9600 bps
+    uint32_t system_clock = 36000000; // 16 MHz (HSI)
+    uint32_t baud_rate = 9600;
+    USART2->BRR = (system_clock + (baud_rate / 2)) / baud_rate; // Correct BRR calculation
+
+	// Enable Uart Transmit
+	USART2->CR1 |= 8;
+
+	// Enable Uart Recive
+	USART2->CR1 |= 4;
+
+	// Enable Uart
+	USART2->CR1 |= 0x2000;
+
+	//set word length
+	USART2->CR1 |= 0x1000;
+
+while(1){
+
+		char chat = 'A';
+
+	// wait until data transmission is complete
+		while(!(USART2->SR & (1<<6)))
+		{};
+
+		// send data to data register
+		USART2->DR = chat ;
+
+	}
+}
+
